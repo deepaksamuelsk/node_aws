5f07b52b221fcb83c8d3a1d79130d3b5cbf93911
deepaksamuelsk <53965916+deepaksamuelsk@users.noreply.github.com>
2019-08-10 13:43:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+var config = require('./config');
+var port = process.env.PORT || 3000;
+var val = require('./validation/validation');
+var AWS = require('aws-sdk');
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+const Cryptr = require('cryptr');
+const objCryptr = new Cryptr(config.Kinesis.cryptkey);
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ extended: true }))
+
+AWS.config.update({
+    region: config.Kinesis.region,
+    accessKeyId: config.Kinesis.accessKeyId,
+    secretAccessKey: config.Kinesis.secretAccessKey
+});
+
+app.post('/t', function (req, res) {
+
+    var resContent = {
+        "status": "",
+        "response": "",
+    }
+
+    try {
+        var firehose = new AWS.Firehose();
+        var bodyContent = JSON.stringify(req.body);
+        var objRetValid = val.validatejson(req.body);
+
+        if (objRetValid.validjson == true) {
+        res.contentType = "application/json";
+        var params = {
+            DeliveryStreamName: config.Kinesis.streamname,
+            Record: {
+                Data: bodyContent
+            }
+        };
+        firehose.putRecord(params, function (err, data) {
+                if (err) {
+                    resContent.status = "Error in Kinesis api";
+                    resContent.response = err;
+                }
+                else {
+                    resContent.status = "Ok";
+                    resContent.response = data;
+                }
+                res.send(resContent);
+            });
+        } else {
+            resContent.status = "Error";
+            resContent.response = objRetValid.description;
+            res.send(resContent);
+        }  
+    }
+    catch (ex) {
+        resContent.status = "Error";
+        resContent.response = ex;
+        res.send(resContent);
+    }
+});
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Server started...')
+})
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {};
+config.Kinesis = {};
+config.Kinesis.region = 'ap-south-1';
+config.Kinesis.streamname = 'apinew';
+config.Kinesis.accessKeyId = 'AKIAJG4CO7VTY4JIIEWA';
+config.Kinesis.secretAccessKey = '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';
+config.Kinesis.cryptkey = 'Admin@#2019';
+module.exports = config;
